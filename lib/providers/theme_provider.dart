14d80522d45f18b84c4ef9a4336211a99fa81602
shravan-vasala <shravanvasala999@gmail.com>
2026-08-01 16:48:49 +0530

    Fix bug 017: use sharedPreferencesProvider in theme_provider

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'app_providers.dart';
+
 final themeModeProvider = StateNotifierProvider<ThemeModeNotifier, ThemeMode>((ref) {
-  return ThemeModeNotifier();
+  final prefs = ref.watch(sharedPreferencesProvider);
+  return ThemeModeNotifier(prefs);
 });
 
 class ThemeModeNotifier extends StateNotifier<ThemeMode> {
-  ThemeModeNotifier() : super(ThemeMode.system) {
+  final SharedPreferences _prefs;
+
+  ThemeModeNotifier(this._prefs) : super(ThemeMode.system) {
     _loadTheme();
   }
 
-  Future<void> _loadTheme() async {
-    final prefs = await SharedPreferences.getInstance();
-    final modeStr = prefs.getString('themeMode') ?? 'system';
+  void _loadTheme() {
+    final modeStr = _prefs.getString('themeMode') ?? 'system';
     state = ThemeMode.values.firstWhere(
       (e) => e.name == modeStr,
       orElse: () => ThemeMode.system,
@@ -21,8 +25,7 @@ class ThemeModeNotifier extends StateNotifier<ThemeMode> {
   }
 
   Future<void> setThemeMode(ThemeMode mode) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('themeMode', mode.name);
+    await _prefs.setString('themeMode', mode.name);
     state = mode;
   }
 }
